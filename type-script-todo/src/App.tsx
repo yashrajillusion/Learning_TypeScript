c78db847d9503bb9f430ffef9efd5435644e1d3c
yashrajillusion <95868808+yashrajillusion@users.noreply.github.com>
2022-06-01 11:12:19 +0530

    Initiated a React app with typeScript

---
+++ type-script-todo/src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+
+const App: React.FC = () => {
+  const [count, setCount] = useState(0);
+
+  return <div className="App">Yash</div>;
+};
+
+export default App;
