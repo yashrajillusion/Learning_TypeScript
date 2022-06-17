7809de69da1937e684195d4f4c9fccb7f3cd4bd4
yashrajillusion <95868808+yashrajillusion@users.noreply.github.com>
2022-06-17 23:03:22 +0530

    Learn patch auth sorting in firebase

--- firebase/src/Config/firebase.js
+++ firebase/src/Config/firebase.js
@@ -12,8 +12,16 @@ import {
   where,
   orderBy,
   serverTimestamp,
+  getDoc,
+  updateDoc,
 } from "firebase/firestore";
-
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 const firebaseConfig = {
   apiKey: "AIzaSyC02_hdO043ziiIedAIMCz_XbkgvgYiOHc",
   authDomain: "yashrajillusion2.firebaseapp.com",
@@ -47,16 +55,14 @@ try {
 // let post = await addDoc(collRef, { name: "yash" });
 
 //first is db second is collection third is id that you wan't to delete
-const docRef = doc(db, "orders", "b7XPF9UwtpcV86BdVhaX");
-let deletereq = await deleteDoc(docRef);
+const docRef = doc(db, "orders", "11rL5LUH45dV8YbEW9qX");
+// let deletereq = await deleteDoc(docRef);
 //this is deleting the post
 
 //realtiem
-const q = query(
-  collRef,
-  where("name", "==", "yash"),
-  orderBy("restaurantName", "desc")
-);
+const q = query(collRef, orderBy("createdAt"));
+// where("name", "==", "yash")
+//orderBy("name")
 onSnapshot(q, (snap) => {
   let data = [];
   try {
@@ -74,4 +80,53 @@ onSnapshot(q, (snap) => {
 //queries
 // const q = query(collRef, where('name', '==', "yash"))
 
+//get a single item
+// let res1 = await getDoc(docRef);
+onSnapshot(docRef, (doc) => {
+  //every time listen if user update their data as well
+  console.log(doc.data());
+});
+
+//update document
+let res1 = await updateDoc(docRef, {
+  restaurantName: "Maha Laxmi Sweets",
+});
+
+//firebase auth
+// getAuth()
+const auth = getAuth();
+/*
+try {
+  let res2 = await createUserWithEmailAndPassword(
+    auth,
+    "yskyrakjk2@gmail.com",
+    "123456"
+  );
+  console.log(res2.user);
+} catch (error) {
+  console.log(error.message);
+}
+*/
+
+//logout
+/*
+try {
+  let res3 = await signOut(auth);
+} catch (e) {
+  console.log(e.message);
+}
+*/
+//sign in
+
+/*
+signInWithEmailAndPassword(auth, "skyrakjk2@gmail.com", "123456")
+.then((cred) => console.log(cred))
+.catch((e) => console.log(e.message));
+*/
+
+onAuthStateChanged(auth, (user) => {
+  console.log(user);
+});
+
+//for unsubcribe just we need to assign a variable to subscribe that retun function we have to invoke it
 export { app };
